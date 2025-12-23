cfe430cb30aa1f35165262096eaaceecfb3bc2f9
Adelaida-Viliorovna <adelaida.viliorovna@gmail.com>
2025-12-23 13:19:17 +0200

    test commit №3

--- src/main/java/com/ua/rush/modul2/Main.java
+++ src/main/java/com/ua/rush/modul2/Main.java
@@ -2,6 +2,6 @@ package com.ua.rush.modul2;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Module 2 is under construction.");
+        System.out.println("Module 2");
     }
 }
