efa9234df2e206f145bc06574aa12b5fcd2fd14f
Adelaida-Viliorovna <adelaida.viliorovna@gmail.com>
2025-12-25 17:28:12 +0200

    Add Location class with animals and plants

--- src/main/java/com/ua/rush/modul2/model/location/Location.java
+++ src/main/java/com/ua/rush/modul2/model/location/Location.java
@@ -1,4 +1,50 @@
 package com.ua.rush.modul2.model.location;
 
+import com.ua.rush.modul2.model.animal.Animal;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Location {
+
+    private final List<Animal> animals = new ArrayList<>();
+    private int plantCount;
+
+    public Location(int initialPlantCount) {
+        this.plantCount = initialPlantCount;
+    }
+
+    // --- animals ---
+    public void addAnimal(Animal animal) {
+        animals.add(animal);
+    }
+
+    public void removeAnimal(Animal animal) {
+        animals.remove(animal);
+    }
+
+    public List<Animal> getAnimals() {
+        return Collections.unmodifiableList(animals);
+    }
+
+    // --- plants ---
+    public int getPlantCount() {
+        return plantCount;
+    }
+
+    public void addPlants(int amount) {
+        if (amount > 0) {
+            plantCount += amount;
+        }
+    }
+
+    public void removePlants(int amount) {
+        if (amount > 0) {
+            plantCount -= amount;
+            if (plantCount < 0) {
+                plantCount = 0;
+            }
+        }
+    }
 }
