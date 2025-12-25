6bdfc5a43f69ce2a8feceb0856fd1ae35f5c7789
Adelaida-Viliorovna <adelaida.viliorovna@gmail.com>
2025-12-25 17:32:54 +0200

    Add Island class with grid of locations

--- src/main/java/com/ua/rush/modul2/island/Island.java
+++ src/main/java/com/ua/rush/modul2/island/Island.java
@@ -1,4 +1,43 @@
 package com.ua.rush.modul2.island;
 
+import com.ua.rush.modul2.model.location.Location;
+
 public class Island {
+
+    private final int width;
+    private final int height;
+    private final Location[][] locations;
+
+    public Island(int width, int height, int initialPlantCount) {
+        this.width = width;
+        this.height = height;
+        this.locations = new Location[width][height];
+
+        initLocations(initialPlantCount);
+    }
+
+    private void initLocations(int initialPlantCount) {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                locations[x][y] = new Location(initialPlantCount);
+            }
+        }
+    }
+
+    public Location getLocation(int x, int y) {
+        if (x < 0 || x >= width || y < 0 || y >= height) {
+            throw new IndexOutOfBoundsException(
+                    "Invalid location coordinates: x=" + x + ", y=" + y
+            );
+        }
+        return locations[x][y];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
 }
