7e107871f8ab399554d959709f2a2329a6a204f7
Adelaida-Viliorovna <adelaida.viliorovna@gmail.com>
2025-12-25 17:20:48 +0200

    Add Plant base class

--- src/main/java/com/ua/rush/modul2/model/plant/Plant.java
+++ src/main/java/com/ua/rush/modul2/model/plant/Plant.java
@@ -1,4 +1,21 @@
 package com.ua.rush.modul2.model.plant;
 
 public class Plant {
+
+    private final double weight;
+    private final int maxOnLocation;
+
+    public Plant(double weight, int maxOnLocation) {
+        this.weight = weight;
+        this.maxOnLocation = maxOnLocation;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public int getMaxOnLocation() {
+        return maxOnLocation;
+    }
 }
+
